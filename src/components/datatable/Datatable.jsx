236ed16d8de78be7956f6e8a57634d430ea0bf59
Adithya Saish <adiwithmj@gmail.com>
2022-07-09 13:04:55 +0530

    added function to handle deletion of an item in data table

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userRows, userColumns } from "../../datatablesource";
 import { Link } from "react-router-dom";
 
 function Datatable() {
+  const [data, setData] = useState(userRows);
+  function handleDelete(id) {
+    setData(data.filter((item) => item.id !== id));
+  }
   const actionColumn = [
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: () => {
+      renderCell: (params) => {
         return (
           <div className="cellAction">
             <Link to="/users/test" style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div className="deleteButton">Delete</div>
+            <div
+              className="deleteButton"
+              onClick={() => {
+                handleDelete(params.row.id);
+              }}
+            >
+              Delete
+            </div>
           </div>
         );
       },
@@ -36,7 +47,7 @@ function Datatable() {
       </div>
       <DataGrid
         className="datagrid"
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
         pageSize={8}
         rowsPerPageOptions={[8]}
